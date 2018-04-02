c19d3db2afe5ac6d61cd5909c55181e4d85556b0
Jabrils <ankelzspam@gmail.com>
2018-04-02 00:38:34 -0700

    most functionality is there

--- sketch.js
+++ sketch.js
@@ -6,15 +6,26 @@ This uses a pre-trained model on a corpus of Hemingway
 === */
 
 // Create the LSTM Generator passing it the model directory
-const lstm = new ml5.LSTMGenerator('models/dbnamegen/', modelReady);
+const lstm = new ml5.LSTMGenerator('model', modelReady);
 
 let textInput;
 let lengthSlider;
 let tempSlider;
 let button;
 
+function makeid() {
+  var text = "";
+  var possible = "_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var size = random(1,10);
+
+  for (var i = 0; i < size; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
 function modelReady() {
-  select('#status').html('Model Loaded');
+  ToggleButton(true,"Generate!");
 }
 
 function setup() {
@@ -22,41 +33,43 @@ function setup() {
 
   // Grab the DOM elements
   textInput = select('#textInput');
-  lengthSlider = select('#lenSlider');
-  tempSlider = select('#tempSlider');
   button = select('#generate');
+  ToggleButton(false,"Can't Load!");
 
   // DOM element events
   button.mousePressed(generate);
-  lengthSlider.input(updateSliders);
-  tempSlider.input(updateSliders);
-
-  // Update the slider values
-  function updateSliders() {
-    select('#length').html(lengthSlider.value())
-    select('#temperature').html(tempSlider.value())
-  }
 }
 
 // Generate new text
 function generate() {
   // Update the status log
-  select('#status').html('Generating...');
+  ToggleButton(false,"Generating...");
 
   // Grab the original text
   let original = textInput.value();
   // Make it to lower case
-  let txt = original.toLowerCase();
+  let txt = original.toLowerCase()+',';
+  
+  // var that we will use to send to our network
+  let rTxt = "";
+  
+  // Check if there's something to send
+  if (txt.length > 1) {
+    rTxt = txt;
+  }
+  else
+  {
+    rTxt = makeid()+',';
+  }
 
   // Check if there's something to send
-  if (txt.length > 0) {
     // This is what the LSTM generator needs
     // Seed text, temperature, length to outputs
     // TODO: What are the defaults?
     let data = {
-      seed: txt,
-      temperature: tempSlider.value(),
-      length: lengthSlider.value()
+      seed: rTxt,
+      temperature: random(.7,.9),
+      length: 50
     };
 
     // Generate text with the lstm
@@ -65,8 +78,84 @@ function generate() {
     // When it's done
     function gotData(result) {
       // Update the status log
-      select('#status').html('Ready!');
-      select('#result').html(txt + result.generated);
+      ToggleButton(true,"Generate!");
+      let fullList = ReturnTop3(result.generated);
+      select('#result').html(ListThisArray(fullList));
+      //select('#debug').html(rTxt + result.generated);
     }
   }
+
+  // 
+  function ReturnTop3(s)
+{
+  let fC = FindCaps(s);
+
+  for (let j=0; j<fC.length; j++)
+  {    
+    s = CapitalizeLetter(s,fC[j]);
+  }
+
+  s = AddSpace(s);
+
+  s=s.split(',');
+
+  return s.splice(0,s.length-1);
+}
+
+function ListThisArray(h)
+{
+let ret = "";
+for (let i=0; i<h.length; i++)
+{
+  ret+= h[i]+"<br />";
+}
+return ret;
+}
+
+// 
+function ToggleButton(s,text)
+{
+  if(s)
+  {
+    button.removeAttribute('disabled');  
+  }
+  else
+  {
+    button.attribute('disabled',false);
+  }
+  button.html(text);
+}
+
+// 
+function CapitalizeLetter(s,loc)
+{  
+  ret = "";
+
+  for (let i=0; i<loc; i++)
+  {
+  ret+= s[i];
+  }
+
+  return ret+s.charAt(loc).toUpperCase() + s.slice(loc+1)
+ }
+
+ // 
+function FindCaps(s)
+{
+  arr = [0];
+  for (let i=0; i<s.length; i++)
+  {
+    if(s[i]=='_' || s[i]==',')
+    {
+      arr.push(i+1);
+    }
+  }
+
+  return arr;
+}
+
+// 
+function AddSpace(s)
+{
+  return s.replace(/_/g,' ');
 }
